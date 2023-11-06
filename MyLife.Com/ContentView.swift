9287ccc4c2599daf5b440d580b20fa8aebbf3aa9
IltonFerpe <129966216+IltonFerpe@users.noreply.github.com>
2023-11-06 12:35:45 -0300

    Initial Commit

---
+++ MyLife.Com/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyLife.Com
+//
+//  Created by Ilton Ferpec on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLife.Com/MyLife_ComApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLife_ComApp.swift
+//  MyLife.Com
+//
+//  Created by Ilton Ferpec on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MyLife_ComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
